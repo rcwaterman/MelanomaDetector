2a9d822a060ac6dd6d25b63530ef677a4a0d31dd
Ryan <watermanrw@gmail.com>
2024-04-13 19:09:47 -0400

    Functional

--- python/app/Melanoma/melanoma_app.py
+++ python/app/Melanoma/melanoma_app.py
@@ -3,16 +3,19 @@ Test script for the benign/malignant classification model. Select a dir,
 """
 
 from tkinter import filedialog
+from PIL import Image
 import os
-import cv2
+import numpy as np
 import torch
 import torch.nn as nn
-from torchvision import models,transforms
+from torchvision import models,transforms, datasets
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
 #Paths
 image_dir = filedialog.askdirectory()
 image_list = [os.path.join(image_dir,file) for file in os.listdir(image_dir)]
-state_dict_path = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), r'models\Melanoma\melanoma_vgg.pt')
+model_path = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), r'models\Melanoma\melanoma_vgg.pt')
 
 #Define image size. Cursory look at data shows image sizes of 224 x 224. Shrink this down to speed up training.
 IMG_SIZE = 168
@@ -45,17 +48,36 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f'Using device : {device}')
     model = create_model(device)
-    model.load_state_dict(state_dict_path)
-    model.eval()
+    model.load_state_dict(torch.load(model_path))
 
-    for image in image_list:
-        img = cv2.imread(image)
-        cv2.imshow("Image", img)
-        img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
-        img = transforms.ToTensor(img)
-        output = model(img)
-        print(output)
-        cv2.waitKey(0)
+    transformer = transforms.Compose([
+            #Load image into tensor and normalize
+            transforms.Resize(size = (IMG_SIZE, IMG_SIZE), antialias = True),
+            transforms.ToTensor(),
+            transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])]
+    )
+    
+    data = datasets.ImageFolder(image_dir, transformer)
+    loader = DataLoader(data, batch_size = 256, shuffle = True)
+
+    # Display image and label.
+    inputs, labels = next(iter(loader))
+    input, label = inputs.to(device), labels.to(device)
+    output = model(input)
+
+    running_accuracy = []
+
+    for idx,pred in enumerate(output):
+        img = inputs[idx].T
+        new_img = img
+        predicted = (torch.sigmoid(pred) > 0.5).float()
+        if predicted == label[idx]:
+            running_accuracy.append(1)
+        else:
+            running_accuracy.append(0)
+        print(f'Prediction: {int(predicted[0])}', f"Label: {label[idx]}", f'Running Accuracy: {round((sum(running_accuracy)/len(running_accuracy))*100, 2)}%')
+        plt.imshow(new_img)
+        plt.show()
 
 if __name__ == '__main__':
     main()
