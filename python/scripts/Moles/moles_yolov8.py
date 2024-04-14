77d56bfa5b13b5a265853276fe9cfddb7266bc87
Ryan <watermanrw@gmail.com>
2024-04-14 17:00:13 -0400

    Adding new models.

--- python/scripts/Moles/moles_yolov8.py
+++ python/scripts/Moles/moles_yolov8.py
@@ -1,3 +1,8 @@
 from ultralytics import YOLO
+import os
 
-yolo = YOLO('yolov8n.pt')
+model = YOLO('yolov8m.pt')
+
+yaml_file = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), r'datasets\Moles\mole_data\yaml.yaml')
+
+results = model.train(data=yaml_file, epochs=100, imgsz=640, verbose=True, device='cuda', optimizer='Adam')
