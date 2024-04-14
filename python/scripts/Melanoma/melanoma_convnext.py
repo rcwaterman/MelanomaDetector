f3f7b80f2893941824fdb112d4ad6e0c783d0e4b
Ryan <watermanrw@gmail.com>
2024-04-13 21:44:23 -0400

    modified:   melanoma_convnext.py

--- python/scripts/Melanoma/melanoma_convnext.py
+++ python/scripts/Melanoma/melanoma_convnext.py
@@ -3,18 +3,18 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
-from torchvision.models import convnext_large, ConvNeXt_Large_Weights
+from torchvision.models import convnext_small, ConvNeXt_Small_Weights
 from torch.utils.data import DataLoader, random_split
 from torch.cuda.amp import autocast, GradScaler
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 #Define image size. Cursory look at data shows image sizes of 224 x 224. Shrink this down to speed up training.
-IMG_SIZE = 196
+IMG_SIZE = 128
 
 def create_model(device):
     #Instantiate the VGG model with default weights
-    model = convnext_large(weights=ConvNeXt_Large_Weights.IMAGENET1K_V1)
+    model = convnext_small(weights=ConvNeXt_Small_Weights.IMAGENET1K_V1)
 
     #Uncomment to print model structure
     #This is necessary for modifying the classification layer
@@ -26,7 +26,7 @@ def create_model(device):
     """
 
     # Modifying final classifier layer
-    model.classifier[2] = nn.Linear(model.classifier[2].out_features, 1)
+    model.classifier[2] = nn.Linear(model.classifier[2].in_features, 1)
 
     #Uncomment to print model structure
     #This is necessary for modifying the classification layer
@@ -99,7 +99,7 @@ def main():
     val_data = datasets.ImageFolder(val_path, val_transformer)
 
     #Define the training parameters and load the data
-    batch_size = 256
+    batch_size = 128
     print("Loading Data...")
     trainLoader = DataLoader(train_data, batch_size = batch_size, shuffle = True, num_workers = 4)
     valLoader = DataLoader(val_data, batch_size = batch_size, shuffle = False, num_workers = 4)
