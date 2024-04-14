83a8599d1525caee7990e385c072708d2c171ab9
Ryan <watermanrw@gmail.com>
2024-04-13 20:24:54 -0400

    Updated structure

--- python/app/Melanoma/melanoma_app.py
+++ python/app/Melanoma/melanoma_app.py
@@ -3,11 +3,11 @@ Test script for the benign/malignant classification model. Select a dir,
 """
 
 from tkinter import filedialog
-from PIL import Image
 import os
-import numpy as np
 import torch
 import torch.nn as nn
+import time
+import numpy as np
 from torchvision import models,transforms, datasets
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
@@ -49,6 +49,7 @@ def main():
     print(f'Using device : {device}')
     model = create_model(device)
     model.load_state_dict(torch.load(model_path))
+    model.eval()
 
     transformer = transforms.Compose([
             #Load image into tensor and normalize
@@ -57,27 +58,40 @@ def main():
             transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])]
     )
     
+    print("Loading dataset...")
     data = datasets.ImageFolder(image_dir, transformer)
-    loader = DataLoader(data, batch_size = 256, shuffle = True)
-
+    loader = DataLoader(data, batch_size = 1000, shuffle = True)
+    print("Load complete")
     # Display image and label.
-    inputs, labels = next(iter(loader))
-    input, label = inputs.to(device), labels.to(device)
-    output = model(input)
-
+    inputs, labels = iter(loader)
+    
+    avg_time = []
     running_accuracy = []
 
-    for idx,pred in enumerate(output):
-        img = inputs[idx].T
-        new_img = img
-        predicted = (torch.sigmoid(pred) > 0.5).float()
-        if predicted == label[idx]:
-            running_accuracy.append(1)
-        else:
-            running_accuracy.append(0)
-        print(f'Prediction: {int(predicted[0])}', f"Label: {label[idx]}", f'Running Accuracy: {round((sum(running_accuracy)/len(running_accuracy))*100, 2)}%')
-        plt.imshow(new_img)
-        plt.show()
+    for input, label in inputs, labels:
+        for i in range(0, input.size(0)-1, 1):
+            inp, lab = input[i].unsqueeze(0), label[i]
+            invTrans = transforms.Compose([ transforms.Normalize(mean = [ 0., 0., 0. ],
+                                                     std = [ 1/0.229, 1/0.224, 1/0.225 ]),
+                                            transforms.Normalize(mean = [ -0.485, -0.456, -0.406 ],
+                                                                std = [ 1., 1., 1. ]),
+                                            ])
+            inp = invTrans(inp)
+            img = inp.squeeze(0).mT
+            inp, lab = inp.to(device), lab.to(device)
+            start = time.time()
+            pred = model(inp)
+            end = time.time()
+            pred_time = end - start
+            avg_time.append(pred_time)
+            predicted = (torch.sigmoid(pred) > 0.5).float()
+            if predicted == lab:
+                running_accuracy.append(1)
+            else:
+                running_accuracy.append(0)
+            print(f'Prediction: {int(predicted[0])}', f"Label: {lab}", f'Evaluation Time: {sum(avg_time)/len(avg_time)}',f'Running Accuracy: {round((sum(running_accuracy)/len(running_accuracy))*100, 2)}%')
+            plt.imshow(img)
+            plt.show()
 
 if __name__ == '__main__':
     main()
